855788d604b15e8f7b7cb00d36a72773cdae6261
Arash <arash_raji@yahoo.com>
2021-04-18 14:05:49 +0200

    Task 3 change component color on click event

--- src/components/component-a.js
+++ src/components/component-a.js
@@ -3,7 +3,8 @@ var template = function(props) {
 	<div id="component-a" class="component">
 	<div class="component-a-container">
 	<span class="component-title text-center"> Component A </span>
-	</div>
+    <button class="btn component-a-btn">change color</button>
+    </div>
 	</div>
 	`;
 };
@@ -24,4 +25,26 @@ css(component_a, {
     display: 'flex',
     'justify-content': 'center',
     'align-items': 'center',
+});
+
+//task 3
+const component_a_container = document.querySelector('.component-a-container');
+css(component_a_container, {
+    display: 'flex',
+    'flex-direction': 'column'
+});
+
+window.addEventListener('load', (event) => {
+
+    //task 3
+    const component_a_btn = document.querySelector('.component-a-btn');
+    component_a_btn.addEventListener('click', changeColor);
+
+    function changeColor(e) {
+        const component_a = document.querySelector('#component-a');
+        const component_b = document.querySelector('#component-b');
+        component_a.classList.add("bg-lightpink");
+        component_b.classList.remove("bg-lightpink");
+    }
+
 });
--- src/components/component-b.js
+++ src/components/component-b.js
@@ -4,6 +4,7 @@ var template = function(props) {
 	<div id="component-b" class="component">
     <div class="component-b-container">
 	<span class="component-title text-center"> Component B </span>
+    <button class="btn component-b-btn">change color</button>
     </div>
 	</div>
     `;
@@ -25,4 +26,28 @@ css(component_b, {
     display: 'flex',
     'justify-content': 'center',
     'align-items': 'center',
+});
+
+//task 3
+const component_b_container = document.querySelector('.component-b-container');
+css(component_b_container, {
+    display: 'flex',
+    'flex-direction': 'column'
+});
+
+
+window.addEventListener('load', (event) => {
+
+    //task 3
+    const component_b_btn = document.querySelector('.component-b-btn');
+    component_b_btn.addEventListener('click', changeColor);
+
+    function changeColor(e) {
+        const component_b = document.querySelector('#component-b');
+        const component_a = document.querySelector('#component-a');
+        component_b.classList.add("bg-lightpink");
+        component_a.classList.remove("bg-lightpink");
+    }
+
+
 });
